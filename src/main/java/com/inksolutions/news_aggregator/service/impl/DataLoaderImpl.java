58d72c845b7cbd5fa273a46e483e15c32eb81ae2
Piotr Kaliszuk <p_kaliszuk@tlen.pl>
2024-11-05 02:11:24 +0100

    old version

--- src/main/java/com/inksolutions/news_aggregator/service/impl/DataLoaderImpl.java
+++ src/main/java/com/inksolutions/news_aggregator/service/impl/DataLoaderImpl.java
@@ -5,21 +5,16 @@ import com.inksolutions.news_aggregator.repository.CityRepository;
 import com.inksolutions.news_aggregator.service.DataLoaderService;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Service
 @RequiredArgsConstructor
 public class DataLoaderImpl implements DataLoaderService, CommandLineRunner {
     private final CityRepository cityRepository;
-    private final EntityManagerFactory entityManagerFactory;
 
     @Override
     public void run(String... args) throws Exception {
@@ -32,7 +27,6 @@ public class DataLoaderImpl implements DataLoaderService, CommandLineRunner {
             return;
         }
 
-        ExecutorService executorService = Executors.newFixedThreadPool(13);
         try (CSVReader csvReader = new CSVReader(new InputStreamReader(getClass().getResourceAsStream("/cities.csv")))) {
             String[] record;
             while ((record = csvReader.readNext()) != null) {
@@ -40,30 +34,12 @@ public class DataLoaderImpl implements DataLoaderService, CommandLineRunner {
                     throw new IllegalArgumentException("City name is missing");
                 }
 
-                final String cityName = record[3];
-
-                executorService.submit(() -> {
-                    EntityManager entityManager = entityManagerFactory.createEntityManager();
-                    try {
-                        City city = new City();
-                        city.setName(cityName);
-                        entityManager.getTransaction().begin();
-                        entityManager.persist(city);
-                        entityManager.getTransaction().commit();
-                    } catch (Exception e) {
-                        if (entityManager.getTransaction().isActive()) {
-                            entityManager.getTransaction().rollback();
-                        }
-                        e.printStackTrace();
-                    } finally {
-                        entityManager.close();
-                    }
-                });
+                City city = new City();
+                city.setName(record[3]);
+                cityRepository.save(city);
             }
         } catch (IOException | CsvValidationException e) {
             throw new RuntimeException(e);
-        } finally {
-            executorService.shutdown();
         }
     }
 }
